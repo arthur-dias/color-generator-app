147ed558b1ba4edfd947a879efba5ac969fc3107
Arthur Dias <dias.arthurr@gmail.com>
2022-03-23 08:07:56 -0300

    changed title and default color picked

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
             type='text'
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder='#f15025'
+            placeholder='#8c1773'
             className={`${error ? 'error' : null}`}
           />
           <button className='btn' type='submit'>
